31f2bde80ba2c49d2412c11adc40d7e35a7e8ad9
Noel Paing Oak Soe <u6530183@au.edu>
2024-06-03 21:48:31 +0700

    Feat: Created Recommendation page

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Card from './Card'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  flex: 1;
+`
 
 const Recommendation = () => {
-  return <div>Recommendation</div>
+  return (
+    <Container>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Container>
+  )
 }
 
 export default Recommendation
