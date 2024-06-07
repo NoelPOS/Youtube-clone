85b3939608c9bc25a9dac9bdd345317c6b2d57a6
Noel Paing Oak Soe <u6530183@au.edu>
2024-06-07 08:50:02 +0700

    Feat: Implemented some userRoutes for backend

---
+++ backend/controllers/user.js
@@ -0,0 +1,5 @@
+import { createError } from '../error.js'
+import User from '../models/User.js'
+import Video from '../models/Video.js'
+
+export const update = (req, res) => {}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
 import cookieParser from 'cookie-parser'
 
 const app = express()
@@ -12,6 +13,17 @@ dotenv.config()
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message || 'Something went wrong'
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('hello!')
